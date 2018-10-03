906dda48171d0e15f2ea28c20fc275ab5f7bb143
nikitamekh <nikita.mekh@gmail.com>
2018-10-03 11:06:21 +0300

    HGCMS-4: Перенесен SimpleAutoloader в core и добавлен класс CmsPlugin - для иницализации всех служебных классов, необходимых для работы core

---
+++ src/esas/hutkigrosh/CmsPlugin.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikit
+ * Date: 02.10.2018
+ * Time: 14:59
+ */
+
+namespace esas\hutkigrosh;
+
+
+use esas\hutkigrosh\utils\Logger;
+use esas\hutkigrosh\utils\SimpleAutoloader;
+
+/**
+ * Class CmsPlugin позволяет удобно настроить и проинициализировать все служебные объекты, неоьходимые для работы core.
+ * Пример использования для CMS Opencart:
+ * (new CmsPlugin())
+ *      ->setCmsPluginDir(dirname(dirname(dirname(__FILE__))))
+ *      ->setComposerVendorDir(dirname(dirname(__FILE__)) . '/vendor')
+ *      ->setRegistry(new RegistryOpencart())
+ *      ->init();
+ * @package esas\hutkigrosh
+ */
+class CmsPlugin
+{
+    private $composerVendorDir;
+    private $cmsPluginDir;
+    /**
+     * @var Registry
+     */
+    private $registry;
+
+    /**
+     * @param string $composerVendorDir
+     * @return CmsPlugin
+     */
+    public function setComposerVendorDir($composerVendorDir)
+    {
+        if (substr($composerVendorDir, -1) == '/')
+            $composerVendorDir = substr($composerVendorDir, 0,  -1);
+        $this->composerVendorDir = $composerVendorDir;
+        return $this;
+    }
+
+    /**
+     * @param mixed $cmsPluginDir
+     * @return CmsPlugin
+     */
+    public function setCmsPluginDir($cmsPluginDir)
+    {
+        $this->cmsPluginDir = $cmsPluginDir;
+        return $this;
+    }
+
+    /**
+     * @param Registry $registry
+     * @return CmsPlugin
+     */
+    public function setRegistry($registry)
+    {
+        $this->registry = $registry;
+        return $this;
+    }
+    
+    public function init() {
+        require_once($this->composerVendorDir . '/autoload.php');
+        Logger::init();
+        $this->registry->init();
+        SimpleAutoloader::register($this->cmsPluginDir);
+    }
+
+}
---
+++ src/esas/hutkigrosh/utils/SimpleAutoloader.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikit
+ * Date: 02.10.2018
+ * Time: 15:03
+ */
+
+namespace esas\hutkigrosh\utils;
+
+
+class SimpleAutoloader
+{
+    private static $rootPath;
+
+    static public function loader($class)
+    {
+        $className = str_replace("\\", DIRECTORY_SEPARATOR, $class);
+        $path = self::$rootPath . '/' . $className . '.php';
+        if (file_exists($path)) {
+            require_once($path);
+            if (class_exists($class)) {
+                return TRUE;
+            }
+        }
+        return FALSE;
+    }
+
+    /**
+     * @param string $rootPath
+     */
+    public static function register($rootPath)
+    {
+        self::$rootPath = $rootPath;
+        spl_autoload_register('SimpleAutoloader::loader');
+    }
+
+}
